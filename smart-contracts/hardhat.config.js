3618043ca2e07229c4274fc9310eb99c28630541
raldblox <raldblox@outlook.com>
2023-10-17 01:02:20 +0800

    added: polygon and scroll network; installed pkgs

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -1,6 +1,44 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-contract-sizer");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {},
+    "mumbai": {
+      timeout: 60000,
+      gasPrice: 50000000000,
+      url: process.env.MUMBAI_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    scrollSepolia: {
+      url: "https://sepolia-rpc.scroll.io/" || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGON_ETHERSCAN,
+    customChains: [
+      {
+        network: "",
+        chainId: 0,
+        urls: {
+          apiURL: "",
+          browserURL: ""
+        }
+      },
+    ]
+  },
 };
