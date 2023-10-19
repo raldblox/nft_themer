36311262753342809d9d81abc4cd8f90f9a11f15
raldblox <raldblox@outlook.com>
2023-10-19 13:19:05 +0800

    update ui

--- web-apps/dapp/components/Hero.js
+++ web-apps/dapp/components/Hero.js
@@ -22,30 +22,30 @@ export default () => {
                         </a>
                     </div>
                 </div>
-                <div className="grid grid-cols-2 gap-16">
+                <div className="grid grid-cols-2 gap-8">
                     <div className="h-full space-y-8 col-span-2 pt-8 p-4 md:p-8 rounded-[25px] md:rounded-[50px] border-2 border-gray-600 bg-[#ffffff39]">
                         <h1 className="text-2xl font-bold text-center">Select Theme</h1>
                         <ul className="grid z-10 rounded-[30px] aspect-square md:aspect-auto overflow-y-scroll content-start md:grid-cols-3 gap-4">
-                            <li className="col-span-1 rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff85]"></li>
-                            <li className="col-span-1 rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff85]"></li>
-                            <li className="col-span-1 rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff85]"></li>
+                            <li className="col-span-1 rounded-[30px] border aspect-square border-1 border-gray-700 bg-white"></li>
+                            <li className="col-span-1 rounded-[30px] border aspect-square border-1 border-gray-700 bg-white"></li>
+                            <li className="col-span-1 rounded-[30px] border aspect-square border-1 border-gray-700 bg-white"></li>
                         </ul>
                     </div>
                     <div className="h-full space-y-8 col-span-2 pt-8 p-4 md:p-8 rounded-[25px] md:ounded-[50px] border-2 border-gray-600 bg-[#ffffff31]">
                         <h1 className="text-2xl font-bold text-center">Select Your NFT</h1>
                         <ul className="grid z-10 aspect-square md:aspect-auto rounded-lg md:rounded-[30px] content-start grid-cols-2 md:grid-cols-6 gap-4 overflow-y-scroll">
-                            <li className="col-span-1 rounded-lg md:rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff50] backdrop-blur-md"></li>
-                            <li className="col-span-1 rounded-lg md:rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff50] backdrop-blur-md"></li>
-                            <li className="col-span-1 rounded-lg md:rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff50] backdrop-blur-md"></li>
-                            <li className="col-span-1 rounded-lg md:rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff50] backdrop-blur-md"></li>
-                            <li className="col-span-1 rounded-lg md:rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff50] backdrop-blur-md"></li>
-                            <li className="col-span-1 rounded-lg md:rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff50] backdrop-blur-md"></li>
-                            <li className="col-span-1 rounded-lg md:rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff50] backdrop-blur-md"></li>
-                            <li className="col-span-1 rounded-lg md:rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff50] backdrop-blur-md"></li>
-                            <li className="col-span-1 rounded-lg md:rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff50] backdrop-blur-md"></li>
-                            <li className="col-span-1 rounded-lg md:rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff50] backdrop-blur-md"></li>
-                            <li className="col-span-1 rounded-lg md:rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff50] backdrop-blur-md"></li>
-                            <li className="col-span-1 rounded-lg md:rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff50] backdrop-blur-md"></li>
+                            <li className="col-span-1 rounded-lg md:rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff] backdrop-blur-md"></li>
+                            <li className="col-span-1 rounded-lg md:rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff] backdrop-blur-md"></li>
+                            <li className="col-span-1 rounded-lg md:rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff] backdrop-blur-md"></li>
+                            <li className="col-span-1 rounded-lg md:rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff] backdrop-blur-md"></li>
+                            <li className="col-span-1 rounded-lg md:rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff] backdrop-blur-md"></li>
+                            <li className="col-span-1 rounded-lg md:rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff] backdrop-blur-md"></li>
+                            <li className="col-span-1 rounded-lg md:rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff] backdrop-blur-md"></li>
+                            <li className="col-span-1 rounded-lg md:rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff] backdrop-blur-md"></li>
+                            <li className="col-span-1 rounded-lg md:rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff] backdrop-blur-md"></li>
+                            <li className="col-span-1 rounded-lg md:rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff] backdrop-blur-md"></li>
+                            <li className="col-span-1 rounded-lg md:rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff] backdrop-blur-md"></li>
+                            <li className="col-span-1 rounded-lg md:rounded-[30px] aspect-square border-1 border-gray-700 bg-[#ffffff] backdrop-blur-md"></li>
                         </ul>
                     </div>
                 </div>
