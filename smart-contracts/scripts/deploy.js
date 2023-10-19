39e46a20eb61745561fd91c54db3c4c0a59d3258
raldblox <raldblox@outlook.com>
2023-10-19 13:45:53 +0800

    added deploy scripts

--- smart-contracts/scripts/deploy.js
+++ smart-contracts/scripts/deploy.js
@@ -1,33 +1,27 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [deployer] = await ethers.getSigners();
+  console.log("Deployer: ", deployer.address);
+  console.log("Balance: ", ethers.utils.formatEther(await deployer.getBalance()), " ETH");
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  // Deploy APE Payment Token
+  const MockedAPE = await hre.ethers.getContractFactory("ERC20Factory");
+  const mockedAPE = await MockedAPE.deploy();
+  await mockedAPE.deployed();
+  console.log("Mocked APE Address: ", mockedAPE.address);
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  // Deploy Themer
+  const Themer = await hre.ethers.getContractFactory("NFTThemer");
+  const themer = await Themer.deploy(mockedAPE.address);
+  await themer.deployed();
+  console.log("NFT Themer address: ", themer.address);
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
